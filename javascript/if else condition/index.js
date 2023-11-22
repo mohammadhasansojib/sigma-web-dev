9d3358f3105537ba355af51a52dac6ae1f45d301
sajib hasan <sajibhasan800080@gmail.com>
2023-11-22 23:55:45 +0600

    added code in index.js

---
+++ javascript/if else condition/index.js
@@ -0,0 +1,18 @@
+let a = 50, b = 3;
+
+
+
+if(a < 12){
+    console.log("a's value is greater than 12.");
+}else if(a == 12){
+    console.log("a's value is equal to 12.")
+}else{
+    console.log("a's value is not greater than 12.");
+}
+
+let c = a < 50 ? "yeah bro" : "nah bro";
+
+let d = !false;
+console.log(d);
+
+console.log(c);
